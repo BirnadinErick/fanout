0d7a7140bce5aac689377de6b2fd56d8a253d87b
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2023-06-02 20:46:27 +0530

    do dashboard partially

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home(){
-  return <h1>Fan Out Dashboard</h1>
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <section className="p-8 space-y-4">
+      <header className="mb-8">
+        <h1 className="text-4xl font-bold">Fan Out</h1>
+        <p className="subtitle">Welcome to the Dashboard!</p>
+      </header>
+
+      <p className="subtitle">Authenticate to further proceeed</p>
+      <div className="space-x-4">
+        <Link href={"/auth"} className="action-button">
+          Sign up{" "}
+        </Link>
+        <Link href={"/auth/login"} className="action-button">
+          Log In
+        </Link>
+      </div>
+    </section>
+  );
 }
